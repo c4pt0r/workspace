6994e3839d7a8ae104e8cb2e6d87c521bdadb55a
dongxu <huang@pingcap.com>
2022-06-01 11:23:58 -0700

    update i3

--- i3/dp-only.sh
+++ i3/dp-only.sh
@@ -1,3 +1,2 @@
 #!/bin/sh
-xrandr --output HDMI-2 --off --output HDMI-1 --off --output DP-1 --primary --mode 3840x2160 --pos 0x0 --rotate normal --output eDP-1 --off --output DP-2 --off
-feh --bg-scale ~/.config/i3/desktop-arch.png
+xrandr --output eDP-1 --off --output DP-1 --primary --mode 3840x2160 --pos 0x0 --rotate normal --output HDMI-1 --off --output DP-2 --off --output HDMI-2 --off --output DP-3 --mode 3840x2160 --pos 3840x0 --rotate normal
